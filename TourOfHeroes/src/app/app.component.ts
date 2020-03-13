a0697212cf6e6bcc6c560e4b458b5da31d78e823
Beatriz <email>
2020-03-13 11:27:04 +0100

    Paso 5

--- TourOfHeroes/src/app/app.component.ts
+++ TourOfHeroes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TourOfHeroes';
+  /*title = 'TourOfHeroes';*/
+
+  ngOnInit() {
+    this.resetHeroes();
+  }
+
+  currentHero: Hero;
+
+  title = 'Template Syntax';
+  name: string = Hero.heroes[0].name;
+  hero: Hero; // defined to demonstrate template context precedence
+  heroes: Hero[];
+
+   // trackBy change counting
+   heroesWithTrackByCountReset = 0;
+
+   // updates with fresh set of cloned heroes
+   resetHeroes() {
+    this.heroes = Hero.heroes.map(hero => hero.clone());
+    this.currentHero = this.heroes[0];
+    this.hero = this.currentHero;
+    this.heroesWithTrackByCountReset = 0;
+  }
 }
+
