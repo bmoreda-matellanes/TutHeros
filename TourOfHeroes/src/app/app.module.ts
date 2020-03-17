f9d3d061b103423817adf39e74e84d3016cbbe89
Beatriz <email>
2020-03-17 12:36:25 +0100

    Paso 10

--- TourOfHeroes/src/app/app.module.ts
+++ TourOfHeroes/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([{path:'',component:ProductListComponent},{path:'products/:productId',component:ProductDetailsComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
